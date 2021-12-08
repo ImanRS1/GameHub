d3c99f007c2ef3d11e978a7c1a0328aecd640696
Iman Reza Soltani <iman.rezasoltani@gmail.com>
2021-12-08 16:19:43 +0100

    CORS middleware added

--- gameServer/gameServer.js
+++ gameServer/gameServer.js
@@ -11,6 +11,12 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
 
 app.get('/api/games/:searchQuery', getGames);
 app.get('/api/home', getHomeData);
