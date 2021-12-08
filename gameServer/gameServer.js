eae581fad66666728160c7d1f17abfd76c87c7f5
Iman Reza Soltani <iman.rezasoltani@gmail.com>
2021-12-08 17:33:31 +0100

    gameServer CORS middleware removed

--- gameServer/gameServer.js
+++ gameServer/gameServer.js
@@ -11,12 +11,6 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-//   next();
-// });
 
 const corsCaller = (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
